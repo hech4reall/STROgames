98975c99012fb924130ec3e5799d35b9fabaf950
hech4reall <116972890+hech4reall@users.noreply.github.com>
2025-07-22 19:26:40 +0100

    Update Accueil.js

--- client/src/comps/Accueil.js
+++ client/src/comps/Accueil.js
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
-import { logout } from "../redux/slices/authSlice" // Corrected path
+import { logout } from "../redux/slices/authSlice" 
 import "./acceuil.css"
 
 function Accueil() {
